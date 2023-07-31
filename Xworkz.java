6659c715c3973f3a6f89913a4b4c21c449aa3f70
unknown <prematc.xworkz@gmail.com>
2023-07-31 11:28:23 +0530

    association related files

--- Xworkz.java
+++
@@ -1,8 +0,0 @@
-//declaration of a class  program
-class Xworkz{
-public static void main(String[] args){
-system.out.println("no body believes me");
-system.out.println("I am prema ");
-system.out.println("hello,how are you");
-}
-}
